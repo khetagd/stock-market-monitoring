3d1d4d8d4f74f3b70e06fb4dd94cc3d4b23474a1
Khetag Dudiev <khsdudiev@edu.hse.ru>
2023-11-25 23:35:41 +0300

    fixed try/except block

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import requests
 bot = telebot.TeleBot('6669067736:AAFld0-siHEvSVl8P3bhbHxh_GUPhV-uLVU')
 
 #data = pd.read_csv('/Users/grigorij/Downloads/digital_currency_list.csv')
-API = '4JN9ZD24ZTMKWX5R'
+APIs = ['4JN9ZD24ZTMKWX5R', 'BHM7WHDX7K66ET61', '8OKWRXIXB7VBMGAP', 'ZG7VF29SB1BSCTVF', 'D9SOYTBRK9ULOSNP']
 
 data = {}
 
@@ -38,25 +38,40 @@ def main(message):
 
 def get_stock_info(message):
     currency = message.text.strip()
+    curr_api_id = 0
     try:
-        url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency}&to_currency=USD&apikey=4JN9ZD24ZTMKWX5R'
+        url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency}&to_currency=USD&apikey={APIs[curr_api_id]}'
         res = requests.get(url)
         data = res.json()
+        try:
+            test = data['Information']
+            curr_api_id += 1
+            url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={currency}&to_currency=USD&apikey={APIs[curr_api_id]}'
+            res = requests.get(url)
+            data = res.json()
+        except:
+            pass
         curr_output = data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]
         rate_output = data["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
         bot.send_message(message.chat.id, f'Курс {currency} к {curr_output}: {rate_output}')
     except:
-        url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={currency}&apikey=4JN9ZD24ZTMKWX5R'
-        res = requests.get(url)
-        data = res.json()
-        curr_output = data["Global Quote"]["01. symbol"]
-        rate_output = data["Global Quote"]["05. price"]
-        bot.send_message(message.chat.id, f'Курс {curr_output} к доллару: {rate_output}')
-    else:
-        bot.send_message(message.chat.id, 'Что-то пошло не так :(')
-@bot.message_handler(commands=['site'])
-def site(message):
-    webbrowser.open('https://www.cbr.ru/currency_base/daily/')
+        try:
+            url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={currency}&apikey={APIs[curr_api_id]}'
+            res = requests.get(url)
+            data = res.json()
+            try:
+                test = data['Information']
+                curr_api_id += 1
+                url = f'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={currency}&apikey={APIs[curr_api_id]}'
+                res = requests.get(url)
+                data = res.json()
+            except:
+                pass
+            curr_output = data["Global Quote"]["01. symbol"]
+            rate_output = data["Global Quote"]["05. price"]
+            bot.send_message(message.chat.id, f'Курс {curr_output} к доллару: {rate_output}')
+        except:
+            bot.send_message(message.chat.id, 'Что-то пошло не так :(')
 
 
 bot.polling(none_stop=True)
